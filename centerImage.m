e31b3894e7873a7222048ca97814d8e94a7a7757
joshinils <joshua.laskowski@web.de>
2018-07-12 08:37:07 +0200

    hm

--- centerImage.m
+++ centerImage.m
@@ -1,5 +1,5 @@
 function [ bild ] = centerImage( bild )
 	[H, B]=size(bild);
-	bild = bild(mod(floor(H/2):floor(H*3/2),H)+1, mod(floor(B/2):floor(B*3/2),B)+1);
+	bild = bild(mod(floor(H/2):floor(H*3/2-1),H)+1, mod(floor(B/2):floor(B*3/2-1),B)+1);
 end
 
--- imshowCompl.m
+++ imshowCompl.m
@@ -1,11 +1,14 @@
-function [ p ] = imshowCompl( bild )
+function [ p ] = imshowCompl( bild, titel )
+	if nargin < 2
+		titel='.';
+	end
 	p=subplot(1,2,1);
 	imshow(real(bild),[]);
-	title('re(bild)');
+	title(['re(',titel,')']);
 
 	subplot(1,2,2);
 	imshow(imag(bild),[]);
-	title('im(bild)'); 
+	title(['im(',titel,')']); 
 	figure;
 end
 
